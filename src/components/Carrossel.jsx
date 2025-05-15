00235b9983de22173f73915de78648a7a06b2a84
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-15 09:41:19 -0300

    fix/jcf/ajustes-carrossel

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -12,6 +12,7 @@ export const Carrossel = () => {
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
@@ -25,20 +26,20 @@ export const Carrossel = () => {
                         <img src={ImageCollection1} class="d-block w-100" alt="Tênis Nike Gamma Force (Branco/Roxo?Laranja)" />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
+                            <p>Novo drop supreme.</p>
                         </div>
                     </div>
                     <div className="carousel-item">
                         <img src={ImageCollection2} class="d-block w-100" alt="Tênis Nike Gamma Force (Branco/Roxo?Laranja)" />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
+                            <h5>Coleção Adidas</h5>
                             <p>Some representative placeholder content for the third slide.</p>
                         </div>
                     </div>
                     <div className="carousel-item">
                         <img src={ImageCollection3} class="d-block w-100" alt="Tênis Nike Gamma Force (Branco/Roxo?Laranja)" />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Fourth slide label</h5>
+                            <h5>Novo beat bass</h5>
                             <p>Some representative placeholder content for the fourth slide.</p>
                         </div>
                     </div>
