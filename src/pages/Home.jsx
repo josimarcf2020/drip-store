6474e17e5beacbbfb671e0c9fdd5691b5aa204a6
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-27 21:10:22 -0300

    implementação grupo collections na página inicial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Carrossel } from '../components/Carrossel';
+import Collections from '../components/Collections';
 
 const Home = () => {
     return (
         <div>
             <Carrossel />
+            <Collections />
         </div>
     );
 };
