5e0b8f0e27c902b5678f4812997a501a3a6b5d1a
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-28 21:27:29 -0300

    componentes ProductListing e SpecialOffer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Carrossel } from '../components/Carrossel';
 import Collections from '../components/Collections';
+import { ProductListing } from '../components/ProductListing';
+import SpecialOffer from '../components/SpecialOffer';
+
 
 const HomePage = () => {
     return (
         <div>
             <Carrossel />
             <Collections />
+            <ProductListing />
+            <SpecialOffer />
         </div>
     );
 };
