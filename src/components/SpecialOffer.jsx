4226af374ec0630f4701669dac9cb5b78211b738
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-29 07:34:20 -0300

    View specialOffer

--- src/components/SpecialOffer.jsx
+++ src/components/SpecialOffer.jsx
@@ -8,17 +8,15 @@ const SpecialOffer = () => {
     
     return (
         <section id="special-offer"> {/* Anteriormente 'laye_container' */}
-            <div id="special-offer-image-wrapper"> {/* Anteriormente 'circle' */}
-                <div id="special-offer-image-container"> {/* Anteriormente 'laye_image' */}
-                    <img src={ ImageElipse } alt="Elipse em degradê" id='special-offer-image-elipse'/>
-                    <img src={ ImageLaye } alt="Tênis Air Jordan em oferta especial" id="special-offer-product-image" />
-                </div>
+            <div id="special-offer-image-container">
+                <img src={ ImageElipse } alt="Elipse em degradê" id='special-offer-image-elipse'/>
+                <img src={ ImageLaye } alt="Tênis Air Jordan em oferta especial" id="special-offer-product-image" />
             </div>
             <div id="special-offer-content"> {/* Anteriormente 'special_ofer' */}
-                <p>Oferta especial</p>
-                <h2 className="special-offer-title">Air Jordan edição de colecionador</h2>
-                <p className="special-offer-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et architecto iste minima quam iure laboriosam modi, fugit facilis reprehenderit. A sequi molestiae, eveniet magni voluptates obcaecati dolores magnam aliquam asperiores.</p>
-                <button type="button" className="special-offer-button">Ver Oferta</button>
+                <p id='special-offer-title'>Oferta especial</p>
+                <h2 id='special-offer-title-destak' className="special-offer-title">Air Jordan edição de colecionador</h2>
+                <p id='special-offer-description' className="special-offer-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et architecto iste minima quam iure laboriosam modi, fugit facilis reprehenderit. A sequi molestiae, eveniet magni voluptates obcaecati dolores magnam aliquam asperiores.</p>
+                <button id='special-offer-button' type="button" className="special-offer-button">Ver Oferta</button>
             </div>
         </section>
     )
